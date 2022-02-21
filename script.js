1edd43889e81b2b4e0dd405b8052f0e2acb951b2
Roy Dyck <dyck.ro@gmail.com>
2022-02-20 20:58:33 -0600

    Initialize

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function computerPlay() {
+    let throwNum = Math.floor(Math.random() * 3) + 1;
 
+    switch(throwNum) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+        default:
+            return "Rock";
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+    console.log(computerPlay());
+}
